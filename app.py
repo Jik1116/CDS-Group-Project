e6e83d9bea9855482e3ef36b9f5c400d4a496e36
loypekyong <loypekyong@gmail.com>
2024-04-13 10:18:22 +0800

    audio

--- app.py
+++ app.py
@@ -22,41 +22,31 @@ import scipy
 import os
 import shutil
 
-st.title('Images/Text to Music generator')
 
-text_input = st.text_area('Enter text here')
-image_input = st.file_uploader('Upload an image')
+   
+device = "cuda" if torch.cuda.is_available else "cpu"
 
-if st.button('Generate Music'):
-    st.write('Generating music...')
-    
-    image_to_text_generated = "This is a generated text from image" #yq model output for image to text
-    st.write(image_to_text_generated)
+model = Blip2ForConditionalGeneration.from_pretrained("ybelkada/blip2-opt-2.7b-fp16-sharded", device_map=device, load_in_8bit=True, torch_dtype=torch.float32)
 
-def load_model():
-   
-   device = "cuda" if torch.cuda.is_available else "cpu"
-   
-   model = Blip2ForConditionalGeneration.from_pretrained("ybelkada/blip2-opt-2.7b-fp16-sharded", device_map=device, load_in_8bit=True, torch_dtype=torch.float32)
-   
-   config = LoraConfig(
-        r=16,
-        lora_alpha=32,
-        lora_dropout=0.05,
-        bias="none",
-        target_modules=["q_proj", "k_proj"]
-        )
-   
-   training_model = get_peft_model(model, config)
-   state_dict = torch.load("model1.pt")
-   training_model.load_state_dict(state_dict)
-   training_model.eval()
-   processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-   
-   image = Image.open("sky.jpg")
+config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias="none",
+    target_modules=["q_proj", "k_proj"]
+    )
 
-   inputs = processor(images=image, return_tensors="pt").to(device, torch.float32)
+training_model = get_peft_model(model, config)
+state_dict = torch.load("model1.pt")
+training_model.load_state_dict(state_dict)
+training_model.eval()
+processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+   
+#    image = Image.open("sky.jpg")
+
+def image_to_music(image_input):
+   inputs = processor(images=image_input, return_tensors="pt").to(device, torch.float32)
    pixel_values = inputs.pixel_values
    
    generated_ids = training_model.generate(pixel_values=pixel_values, max_length=100)
@@ -70,4 +60,19 @@ def load_model():
    prompt = generated_caption
    audio = pipe(prompt, num_inference_steps=10, audio_length_in_s=5.0).audios[0]
    # generated image
-   scipy.io.wavfile.write(f"sky.wav", rate=16000, data=audio)
+   scipy.io.wavfile.write(f"output1.wav", rate=16000, data=audio)
+
+   return generated_caption
+
+st.title('Images/Text to Music generator')
+
+text_input = st.text_area('Enter text here')
+image_input = st.file_uploader('Upload an image')s
+
+if st.button('Generate Music'):
+    st.write('Generating music...')
+    caption = image_to_music(image_input)
+    st.write(caption)
+    st.audio("output1.wav", format="audio/wav")
+
+
