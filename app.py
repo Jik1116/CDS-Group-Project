ac1826d2a89f68ddb712c30ef73e55316beb5387
Isaac Tay <IsaacTay@pm.me>
2024-04-15 15:51:16 +0800

    fix: moved ML models to cache

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from transformers import Blip2Processor, Blip2ForConditionalGeneration
+from transformers import Blip2Processor, Blip2ForConditionalGeneration, BitsAndBytesConfig
 from diffusers import AudioLDMPipeline
 
 import torch
@@ -16,43 +16,59 @@ import scipy
    
 device = torch.device("cuda" if torch.cuda.is_available else "cpu")
 
-model = Blip2ForConditionalGeneration.from_pretrained("ybelkada/blip2-opt-2.7b-fp16-sharded", device_map=device, load_in_8bit=True)
-
-config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    lora_dropout=0.05,
-    bias="none",
-    target_modules=["q_proj", "k_proj"]
+@st.cache_resource
+def get_blip_model():
+    quant_config = BitsAndBytesConfig(
+        load_in_8bit=True,
+    )
+    model = Blip2ForConditionalGeneration.from_pretrained("ybelkada/blip2-opt-2.7b-fp16-sharded", quantization_config=quant_config)
+    config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        target_modules=["q_proj", "k_proj"]
     )
 
-training_model = get_peft_model(model, config)
-state_dict = torch.load("model1.pt")
-training_model.load_state_dict(state_dict)
-training_model.eval()
-processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-   
-#    image = Image.open("sky.jpg")
+    training_model = get_peft_model(model, config)
+    state_dict = torch.load("model1.pt")
+    training_model.load_state_dict(state_dict)
+    training_model.eval()
+
+    return training_model
+
+@st.cache_resource
+def get_processor():
+    processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+    return processor
+
+@st.cache_resource
+def get_audio_pipe():
+    repo_id = "cvssp/audioldm-s-full-v2"
+    pipe = AudioLDMPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
+    pipe = pipe.to("cuda")
+
+    return pipe
+
 
 def image_to_music(image_input):
-   inputs = processor(images=image_input, return_tensors="pt").to(device, torch.float32)
-   pixel_values = inputs.pixel_values
-   
-   generated_ids = training_model.generate(pixel_values=pixel_values, max_length=100)
-   # generated text
-   generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-   
-   repo_id = "cvssp/audioldm-s-full-v2"
-   pipe = AudioLDMPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
-   pipe = pipe.to("cuda")
-   
-   prompt = generated_caption
-   audio = pipe(prompt, num_inference_steps=10, audio_length_in_s=5.0).audios[0]
-   # generated image
-   scipy.io.wavfile.write(f"output1.wav", rate=16000, data=audio)
+    processor = get_processor()
+    inputs = processor(images=image_input, return_tensors="pt").to(device, torch.float32)
+    pixel_values = inputs.pixel_values
 
-   return generated_caption
+    blip_model = get_blip_model()
+    generated_ids = blip_model.generate(pixel_values=pixel_values, max_length=100)
+    # generated text
+    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    pipe = get_audio_pipe()
+
+    prompt = generated_caption
+    audio = pipe(prompt, num_inference_steps=10, audio_length_in_s=5.0).audios[0]
+    # generated music
+    scipy.io.wavfile.write(f"output1.wav", rate=16000, data=audio)
+
+    return generated_caption
 
 st.title('Images/Text to Music generator')
 
@@ -65,5 +81,3 @@ if st.button('Generate Music'):
     caption = image_to_music(image)
     st.write(caption)
     st.audio("output1.wav", format="audio/wav")
-
-
